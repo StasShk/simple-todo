48ff0539058325934aad69c42657e07abe354df0
nimagt nimagt <nimagt@gmail.com>
2016-11-10 18:04:08 +1000

    adding api for users and groups

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ router.register(r'itemlist', views.ItemListViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
